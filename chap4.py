04d58a7bfaf781b7e6313e9861232fdefe8e6c32
Chloe <chloe.jvl@gmail.com>
2025-07-09 17:47:25 +1000

    changement load image

--- chap4.py
+++ chap4.py
@@ -17,13 +17,15 @@ from streamlit_folium import st_folium #carte dans streamlit
 
 # Charge l’image depuis un fichier local
 file_path = "cows.jpeg"
-with open(file_path, "rb") as image_file:
-    image_bytes = image_file.read()
-    exif_img = ExifImage(image_bytes) # Lire les métadonnées exif de l’image avec exif
 
+try:
+    with open(file_path, "rb") as img_file:
+        image_bytes = image_file.read()
+        st.image(image_bytes, caption="Image : cows.jpeg", width=400) # Afficher l’image
+        exif_img = ExifImage(image_bytes) # Lire les métadonnées exif de l’image avec exif
 
-# Afficher l’image
-st.image(image_bytes, caption="Image originale", width=300)
+except FileNotFoundError:
+    st.error("L'image cows.jpeg n'a pas été trouvée.")
 
 
 #formulaire
